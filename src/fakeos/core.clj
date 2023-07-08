11888215a309d2dd41544a2eb264bdda9c36f690
squid233 <60126026+squid233@users.noreply.github.com>
2023-07-08 17:03:27 +0800

    window

--- src/fakeos/core.clj
+++ src/fakeos/core.clj
@@ -17,7 +17,45 @@
 ;
 
 (ns fakeos.core
-  (:import (overrungl.glfw GLFW)))
+  (:import (java.lang.foreign MemorySegment)
+           (overrungl RuntimeHelper)
+           (overrungl.glfw Callbacks GLFW GLFWErrorCallback IGLFWFramebufferSizeFun)
+           (overrungl.opengl GL10C GLLoadFunc$Getter GLLoader)
+           (overrungl.util MemoryStack)))
 
-(if-not (GLFW/init) (throw (IllegalStateException. "Failed to initialize GLFW")))
-(GLFW/terminate)
+(defn -main []
+  (.set (GLFWErrorCallback/createPrint))
+  (RuntimeHelper/check (GLFW/init) "Failed to initialize GLFW")
+
+  (try
+    (GLFW/windowHint GLFW/VISIBLE false)
+    (GLFW/windowHint GLFW/RESIZABLE false)
+    (let [^MemorySegment window (with-open [stack (MemoryStack/stackPush)]
+                                  (GLFW/createWindow stack 640 480 "fakeos" MemorySegment/NULL MemorySegment/NULL))]
+      (RuntimeHelper/check (not (RuntimeHelper/isNullptr window)) "Failed to create the window")
+      (try
+        (GLFW/setFramebufferSizeCallback window (reify IGLFWFramebufferSizeFun (invoke [_ _ width height]
+                                                                                 (GL10C/viewport 0 0 width height))))
+        (if-some [vidMode (GLFW/getVideoMode (GLFW/getPrimaryMonitor))]
+          (let [size (GLFW/getWindowSize window)]
+            (GLFW/setWindowPos window (/ (- (.width vidMode) (.x size)) 2) (/ (- (.height vidMode) (.y size)) 2))))
+
+        (GLFW/makeContextCurrent window)
+        (GLFW/swapInterval 1)
+
+        (GLFW/showWindow window)
+
+        (RuntimeHelper/check (some? (GLLoader/loadConfined
+                                      (reify GLLoadFunc$Getter (get [_ string] (GLFW/ngetProcAddress string)))
+                                      )) "Failed to load OpenGL")
+
+        (GL10C/clearColor 0.0 0.0 0.0 1.0)
+
+        (while (not (GLFW/windowShouldClose window))
+          (GL10C/clear (bit-or GL10C/COLOR_BUFFER_BIT GL10C/DEPTH_BUFFER_BIT))
+          (GLFW/swapBuffers window)
+          (GLFW/pollEvents))
+
+        (finally (Callbacks/free window) (GLFW/destroyWindow window))))
+
+    (finally (GLFW/terminate) (GLFW/setErrorCallback nil))))
